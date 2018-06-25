fcd9f9827794dcac9c489bec8e39130104b574a2
MJ Perez <blueavenger@gmail.com>
2018-06-25 16:45:38 -0700

    Fleshing out GET and POST

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,32 @@
+let ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
+  
+  app.get('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+    const details = { '_id': ObjectID(noteId)};
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+  
+  // When our express app gets a POST to /notes,
   app.post('/notes', (req, res) => {
-    // Create note here
-    console.log(req.body);
-    res.send('Hello!');
+    const note = {
+                  text: req.body.body,
+                  title: req.body.title 
+                };
+                
+    db.collection('notes').insert(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has occurred'});
+      } else {
+        res.send(result.ops[0]);
+      }
+    });
   });
 };
